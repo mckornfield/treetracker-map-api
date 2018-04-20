4feaa0e037a244c693dc53f87c52091a396b1f4c
Matthew Xi <matthew.fullstackgis@gmail.com>
2018-04-20 16:58:17 -0700

    Query for cluster by token

--- index.js
+++ index.js
@@ -21,14 +21,20 @@ const pool = new Pool({
 app.get('/trees', function(req, res){   
   console.log(req);
 
-  let sql = "SELECT 'point' AS type, trees.* FROM trees";
+  let token = req.query['token'];
+  let join = '';
+  if(token) {
+    join = "inner join certificates on trees.certificate_id = certificates.id AND certificates.token = '" + token + "'";
+  }
+
+  let sql = "SELECT 'point' AS type, trees.* FROM trees " + join;
   let query = { 
     text: sql
   }
 
   if (req.query['zoom'] < 14) {
     let zoom = req.query['zoom'];
-    let sql = "SELECT 'cluster' AS type, ST_AsGeoJSON(ST_Centroid(clustered_locations)) centroid, ST_AsGeoJSON(ST_MinimumBoundingCircle(clustered_locations)) circle, ST_NumGeometries(clustered_locations) count FROM ( SELECT unnest(ST_ClusterWithin(estimated_geometric_location, $1)) clustered_locations from trees ) clusters";
+    let sql = "SELECT 'cluster' AS type, ST_AsGeoJSON(ST_Centroid(clustered_locations)) centroid, ST_AsGeoJSON(ST_MinimumBoundingCircle(clustered_locations)) circle, ST_NumGeometries(clustered_locations) count FROM ( SELECT unnest(ST_ClusterWithin(estimated_geometric_location, $1)) clustered_locations from trees " + join + " ) clusters";
     query = {
       text : sql,
       values : [.01]
